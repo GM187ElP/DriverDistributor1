d7b2147e519caeed85aea8b9f0654e9b5b222f8e
Arsalan Rahmanian <arsalan@AccountingPrivate2.taksunpars.local>
2025-09-13 14:26:00 +0330

    Adding Environments To IdesignTime

---
+++ DriverDistributor/Services/AppDbContextFactory.cs
@@ -0,0 +1,40 @@
+﻿using DriverDistributor.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace DriverDistributor.Services;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var env = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Development";
+        var initCatalog = env switch
+        {
+            "Development" => "DriverDistributor_Dev",
+            "Staging" => "DriverDistributor_Staging",
+            _ => "DriverDistributor"
+        };
+
+        string hostIp = "10.11.11.28";
+        bool isLocal = System.Net.Dns.GetHostAddresses("localhost")
+                         .Any(ip => ip.ToString() == hostIp);
+
+        var connString = new SqlConnectionStringBuilder
+        {
+            DataSource = isLocal ? ".,1433" : $"{hostIp},1433",
+            InitialCatalog = initCatalog,
+            UserID = "sa",
+            Password = "Arsalan.1461",
+            TrustServerCertificate = true,
+            MultipleActiveResultSets = true
+        }.ToString();
+
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlServer(connString)
+            .Options;
+
+        return new AppDbContext(options);
+    }
+}
